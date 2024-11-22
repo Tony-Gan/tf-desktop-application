e7e5c6b4f9c3724d419ef74b94aa0999082955be
Tony Gan <tony.gan@transmax.com.au>
2024-11-22 16:12:18 +1000

    refactoring

--- ui/components/tf_group_box.py
+++ ui/components/tf_group_box.py
@@ -1,20 +1,30 @@
-from PyQt6.QtWidgets import QGroupBox, QVBoxLayout, QLayout
+from PyQt6.QtWidgets import QGroupBox, QVBoxLayout, QLayout, QWidget
+from PyQt6.QtCore import pyqtSignal
+
+from ui.components.component_creator_mixin import ComponentCreatorMixin
 
 
-class TFGroupBox(QGroupBox):
+class TFGroupBox(QGroupBox, ComponentCreatorMixin):
+    values_changed = pyqtSignal(dict)
+
     def __init__(self, title, layout_type=QVBoxLayout, object_name='section_frame', parent=None):
-        super().__init__(title, parent)
+        QGroupBox.__init__(self, title, parent)
+        ComponentCreatorMixin.__init__(self)
+
         if not issubclass(layout_type, QLayout):
             raise TypeError(f"Invalid layout_type: {layout_type}. Expected a subclass of QLayout.")
 
-        self.parent = parent
         self.setObjectName(object_name)
-
         self.layout = layout_type(self)
         self.layout.setContentsMargins(5, 5, 5, 5)
         self.layout.setSpacing(5)
+        self.setLayout(self.layout)
 
         self._setup_content()
 
     def _setup_content(self):
-        raise NotImplementedError("Subclasses must implement the _setup_content() method.")
+        pass
+
+    def add_child(self, name: str, child: QWidget) -> None:
+        self._register_component(name, child)
+        self.layout.addWidget(child)
